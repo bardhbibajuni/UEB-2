e59cc372e984a01a7f619c0f688cfa912f06e6df
arditaimeri1 <ardita.imeri3@student.uni-pr.edu>
2026-04-25 22:20:31 +0200

    Update login.php shtova dicka

--- login.php
+++ login.php
@@ -6,7 +6,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     $email = $_POST['email'];
     $password = $_POST['password'];
-
     $sql = "SELECT * FROM users WHERE email='$email'";
     $result = mysqli_query($conn, $sql);
 
