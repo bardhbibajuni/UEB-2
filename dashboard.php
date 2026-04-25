cced308cea59522ec330ebb654c1ec319bb3bbf4
arditaimeri1 <ardita.imeri3@student.uni-pr.edu>
2026-04-26 01:21:01 +0200

    Update dashboard.php pjesa hyrese e dashboardit

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['user'])){
+if(!isset($_SESSION['firstname'])){
     header("Location: login.php");
     exit();
 }
@@ -9,86 +9,18 @@ if(!isset($_SESSION['user'])){
 
 <?php include "header.php"; ?>
 
-<style>
-
-.bg {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    background:
-        radial-gradient(circle at 20% 20%, rgba(99,102,241,0.3), transparent 40%),
-        radial-gradient(circle at 80% 30%, rgba(0,255,255,0.2), transparent 40%),
-        radial-gradient(circle at 50% 80%, rgba(255,0,255,0.2), transparent 50%);
-    filter: blur(100px);
-    z-index: -1;
-}
-
-/* Glow efekti */
-.glow {
-    position: fixed;
-    width: 250px;
-    height: 250px;
-    background: radial-gradient(circle, rgba(99,102,241,0.4), transparent 60%);
-    border-radius: 50%;
-    pointer-events: none;
-    transform: translate(-50%, -50%);
-    z-index: 0;
-}
-
-/* Containeri */
-.dashboard {
-    text-align: center;
-    padding: 100px 20px;
-    color: white;
-}
-
-
-.title {
-    font-size: 3rem;
-    font-weight: 700;
-    background: linear-gradient(90deg, #00ffff, #6366f1, #ff00ff);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    margin-bottom: 10px;
-}
-
-
-.subtitle {
-    color: #9ca3af;
-    margin-bottom: 30px;
-}
-
-/* Logout butoni */
-.logout-btn {
-    padding: 14px 30px;
-    border-radius: 30px;
-    border: none;
-    font-weight: 600;
-    cursor: pointer;
-    color: white;
-    background: linear-gradient(135deg, #ff00ff, #6366f1);
-    transition: 0.3s;
-}
-
-.logout-btn:hover {
-    transform: translateY(-5px) scale(1.05);
-    box-shadow: 0 10px 30px rgba(255,0,255,0.5);
-}
-</style>
-
-<div class="bg"></div>
-<div class="glow" id="glow"></div>
-
 <div class="dashboard">
+
+    <div class="glow" id="glow"></div>
+
     <h1 class="title">
-        Welcome, <?php echo $_SESSION['user']; ?> 🧠
+        Welcome, <?php echo $_SESSION['firstname'] . " " . $_SESSION['lastname']."!" ;?>
     </h1>
 
     <p class="subtitle">
         This is your Brain Boost dashboard.
     </p>
 
-  
 </div>
 
 <script>
