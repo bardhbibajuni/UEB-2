67a7fbe5d5219a4e81bf41f8ba6512d8df8c635c
edisonukshini1 <edison.ukshini@student.uni-pr.edu>
2026-04-26 01:50:18 +0200

    Stilizim

---
+++ admin/includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Brain Boost Admin</title>
+    <!-- We use ../ because admin/includes/ is two levels away from assets/ -->
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="bg"></div>
+    <div class="glow" id="glow"></div>
+    
+    <nav>
+        <div class="logo">Brain Boost</div>
+        <div>
+            <a href="admin.php">Dashboard</a>
+            <a href="users.php">Users</a>
+            <a href="../logout.php" class="logout-btn">Logout</a>
+        </div>
+    </nav>
