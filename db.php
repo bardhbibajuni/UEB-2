58d2be5c40aa10550a2c4a8fd3416890878fd874
arditaimeri1 <ardita.imeri3@student.uni-pr.edu>
2026-04-26 01:23:36 +0200

    Update db.php ///

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// <?php
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
-$conn = mysqli_connect("127.0.0.1", "root", "", "course_platform", 3307);
+// $conn = mysqli_connect("127.0.0.1", "root", "", "course_platform", 3307);
 
-if(!$conn){
-    die("DB ERROR: " . mysqli_connect_error());
-}
+// if(!$conn){
+//     die("DB ERROR: " . mysqli_connect_error());
+// }
 
 
-?>
+// ?>
