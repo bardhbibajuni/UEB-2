e554525ebe531439c0c5e231b3653d34e1735e07
arlindaabehraami <arlinda.behrami2@student.uni-pr.edu>
2026-04-26 10:48:27 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -14,12 +14,12 @@ if(!isset($_SESSION['firstname'])){
     <div class="glow" id="glow"></div>
 
     <h1 class="title">
-        Welcome, <?php echo $_SESSION['firstname'] . " " . $_SESSION['lastname']."!" ;?>
-    </h1>
-
-    <p class="subtitle">
-        This is your Brain Boost dashboard.
-    </p>
+    Welcome, <?php echo "{$_SESSION['firstname']} {$_SESSION['lastname']}!"; ?>
+</h1>
+    
+  <p class="subtitle">
+    Welcome to your Brain Boost dashboard.
+</p>
 
 </div>
 
