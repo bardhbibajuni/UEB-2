0b972410cf357e9c624791ac4ab81199ac053059
arlindaabehraami <arlinda.behrami2@student.uni-pr.edu>
2026-04-26 10:53:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,30 @@
 <?php include "header.php"; ?>
 
 <div class="home-container">
-
     <div>
 
         <h1 class="home-title">
-            Brain Boost 🧠
+             Brain Boost 🧠
         </h1>
 
         <p class="home-text">
-            Learn smarter. Build faster. Think better.
+            Learn smarter.  Build faster.  Think better.
         </p>
 
         <div style="margin-top:35px; display:flex; flex-direction:column; gap:15px; align-items:center;">
-
             <a href="login.php">
+               
                 <button class="btn" style="background: linear-gradient(135deg, #00ffff, #6366f1);">
+                    
                     Login
                 </button>
             </a>
 
             <a href="register.php">
                 <button class="btn" style="background: linear-gradient(135deg, #ff00ff, #6366f1);">
-                    Register
+                   Register
                 </button>
             </a>
-
         </div>
 
     </div>
