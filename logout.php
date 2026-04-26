4aa700b7c0e641b748b2876bc456f7ca011d0def
arlindaabehraami <arlinda.behrami2@student.uni-pr.edu>
2026-04-26 10:47:01 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
+
+// Fshin te gjitha te dhenat e sessionit (log-out i perdoruesit)
 session_destroy();
 
+// E kthen perdoruesin prap te faqja login
 header("Location: login.php");
 exit();
 ?>
