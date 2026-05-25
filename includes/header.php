72c3a9b1695b56237b4e2b9061392fe4cedc1d5f
arditaimeri1 <ardita.imeri3@student.uni-pr.edu>
2026-05-25 15:49:05 +0200

    Enhance session management and dynamic menu in header
    Refactor header.php to improve session handling and menu generation.

--- includes/header.php
+++ includes/header.php
@@ -1,19 +1,47 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Course Platform</title>
-    <link rel="stylesheet" href="../style.css">
-</head>
-<body>
-
 <?php
-session_start();
-require_once __DIR__ . '/../helpers.php';
-
-if (!isset($_SESSION['user'])) {
-    header('Location: ../login.php');
-    exit;
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
 }
+
+if (!defined('ROOT_DIR')) {
+    require_once __DIR__ . '/helpers.php';
+}
+
+
+$html = file_get_contents(__DIR__ . '/header.html');
+
+
+ob_start();
 ?>
 
-<?php include 'nav.php'; ?>
+<a href="index.php">Home</a>
+<a href="courses.php">Courses</a>
+
+<?php if (isset($_SESSION['user'])): ?>
+
+    <a href="my_courses.php">My Courses</a>
+    <a href="dashboard.php">Dashboard</a>
+
+    <?php if ($_SESSION['user']['role'] === 'admin'): ?>
+        <a href="add_course.php">Add Course</a>
+        <a href="admin/admin.php">Admin</a>
+    <?php endif; ?>
+
+    <a href="logout.php" class="logout-btn">Logout</a>
+
+<?php else: ?>
+
+    <a href="login.php">Login</a>
+    <a href="register.php">Register</a>
+
+<?php endif; ?>
+
+<?php
+$menu = ob_get_clean();
+
+
+$html = str_replace('<!-- MENU_PLACEHOLDER -->', $menu, $html);
+
+
+echo $html;
+?>
